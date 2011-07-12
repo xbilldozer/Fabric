12dc223b80dec8a4a63f5645f0e071b11aa3df11
William Alton <bill@stackpointerllc.com>
2011-07-12 13:09:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabric::Application.config.secret_token = '5ee13fa93aabc4701219946e6eb3f05803688dfdf16598c9ca16d4892e8ba3be90867d8920c9cbfd4812f51bad3d58cb8098fee1de43bd7215ed355694e2f6c3'
